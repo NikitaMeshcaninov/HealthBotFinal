172641d8ba2385e02b07d5f1a1007595fa2ad77f
Nikita <meshn@outlook.com>
2016-08-30 13:31:49 +0300

    migrating from swing to web interface

---
+++ src/main/webapp/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "youmail@ya.ru";
+$sitename = "Название сайта";
+
+$email = trim($_GET["email"]);
+$phone = trim($_GET["phone"]);
+$text = trim($_GET["text"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Имя: $email \nТелефон: $phone \nТекст: $text";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
